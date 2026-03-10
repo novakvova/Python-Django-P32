be1dcb0ea47ef5a5ed5a8c98f59da2761f8f9aa0
novakvova <novakvova@gmail.com>
2026-03-10 17:04:24 +0200

    Working is good app

--- djangomvt/atbmvt/users/views.py
+++ djangomvt/atbmvt/users/views.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 from django.shortcuts import redirect, render
 
 from .forms import CustomUserCreationForm
@@ -5,8 +6,12 @@ from .forms import CustomUserCreationForm
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        print("---Зберігаємо дані користувача---")
-        return redirect('')
+        # print("---Зберігаємо дані користувача---")
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            return redirect('homepage')
+        else:
+            messages.success(request, 'Виправте помилки у формі')
     else:
         form = CustomUserCreationForm()
 
