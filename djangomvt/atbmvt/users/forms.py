c953be5ce9cfe8146d1c4e2b68d40e85d3d48c66
novakvova <novakvova@gmail.com>
2026-03-10 17:48:27 +0200

    Working upload user image

--- djangomvt/atbmvt/users/forms.py
+++ djangomvt/atbmvt/users/forms.py
@@ -63,11 +63,11 @@ class CustomUserCreationForm(UserCreationForm):
             raise forms.ValidationError("Паролі не співпадають.")
         return password2
     
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if self.cleaned_data.get('image'):
-            user.image_small = self.cleaned_data['image']
-        if commit:
-            user.save()
-        return user
+    # def save(self, commit=True):
+    #     user = super().save(commit=False)
+    #     user.set_password(self.cleaned_data["password1"])
+    #     if self.cleaned_data.get('image'):
+    #         user.image_small = self.cleaned_data['image']
+    #     if commit:
+    #         user.save()
+    #     return user
--- djangomvt/atbmvt/users/views.py
+++ djangomvt/atbmvt/users/views.py
@@ -9,8 +9,17 @@ def register(request):
         # print("---Зберігаємо дані користувача---")
         form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('homepage')
+            try:
+                user = form.save(commit=False)
+                if 'image' in request.FILES:
+                    image = request.FILES.get("image")
+                    user.image_small = image
+                    user.image_medium = image
+                    user.image_large = image
+                user.save()
+                return redirect('homepage')
+            except Exception as e:
+                messages.error(request, f"Щось пішло не так: {str(e)}")
         else:
             messages.success(request, 'Виправте помилки у формі')
     else:
