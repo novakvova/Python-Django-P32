7001c0229a55d7064d92a3ff4e56c49be28a1434
novakvova <novakvova@gmail.com>
2026-03-17 16:23:33 +0200

    Working is good app

--- djangomvt/atbmvt/products/views.py
+++ djangomvt/atbmvt/products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .forms import ProductForm
+
 # Create your views here.
+def app_product(request):
+    form = ProductForm()
+    return render(request, "add_product.html", {"form": form})
