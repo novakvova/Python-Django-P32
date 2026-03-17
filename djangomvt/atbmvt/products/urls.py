29e30698465f27fa0809127461b4e921bb82ea52
novakvova <novakvova@gmail.com>
2026-03-17 18:05:49 +0200

    Working is good app

--- djangomvt/atbmvt/products/urls.py
+++ djangomvt/atbmvt/products/urls.py
@@ -5,4 +5,5 @@ app_name='products'
 
 urlpatterns = [
     path('add/', views.add_product, name='add_product'),
+    path("upload_temp_image/", views.upload_temp_image, name="upload_temp_image"),
 ]
--- djangomvt/atbmvt/products/views.py
+++ djangomvt/atbmvt/products/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-
+from .models import ProductImage
+from django.views.decorators.csrf import csrf_exempt
 from .forms import ProductForm
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+from django.http import JsonResponse
+import uuid
 
 # Create your views here.
 def add_product(request):
     form = ProductForm()
     return render(request, "add_product.html", {"form": form})
+
+@csrf_exempt
+def upload_temp_image(request):
+    if request.method == "POST":
+        file_key = list(request.FILES.keys())[0]
+        image_file = request.FILES[file_key]
+
+        img = ProductImage()
+        img_image = Image.open(image_file)
+        if img_image.mode in ("RGBA", "P"):
+            img_image = img_image.convert("RGB")
+
+        filename = f"{uuid.uuid4().hex}.webp"
+        buffer = BytesIO()
+        img_image.save(buffer, format="WEBP")
+        buffer.seek(0)
+        img.image.save(filename, ContentFile(buffer.read()), save=True)
+        
+        return JsonResponse({"file_id": img.id})
